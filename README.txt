45517cdd6849bb1ea35e1933892c43f048b5e97c
Gaurav Raj <techw803@gmail.com>
2024-09-20 18:00:06 +0530

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+A quick script to setup kali linux docker container for hacking and recon on Google Cloud.
---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+pull_kali_image() {
+    echo "Pulling the latest Kali Linux Docker image..."
+    docker pull kalilinux/kali-rolling
+}
+
+# Function to run Kali Linux Docker container with custom commands
+run_kali_container() {
+    echo "Running Kali Linux Docker container and installing necessary packages..."
+
+    # Run the Kali Linux container and install custom packages
+    sudo docker run -it kalilinux/kali-rolling /bin/bash -c "
+        apt update && apt upgrade -y && \
+        apt install -y nmap net-tools curl vim git && \
+        echo 'Kali Linux container setup complete with custom packages.' && \
+        bash
+    "
+}
+
+# Main function
+main() {
+    pull_kali_image
+
+    run_kali_container
+}
+
+main
