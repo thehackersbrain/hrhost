c3b5ae103a560fb03495788debdae1c4a67461e4
Gaurav Raj <techw803@gmail.com>
2024-09-21 00:17:00 +0530

    fixed wget issue

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ SRC_URL=$1
 hail_hrs() {
 	git clone https://github.com/thehackersbrain/hrhost.git
 	cd hrhost
-	wget -O $SRC_URL/config.zip && unzip config.zip
+	wget $SRC_URL/config.zip && unzip config.zip
 	docker build -t hrs .
 	docker run -it -v /home/thehackersbrain3/hrhost/recon:/root/recon hrs /bin/bash
 }
